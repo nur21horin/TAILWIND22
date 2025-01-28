46ece2dce3c1dedc7e6a4ef5984025b24db93b31
nur21horin <nurm98371@gmail.com>
2025-01-28 13:02:45 +0600

    intial project done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1E3A8A',
+          secondary: '#4F46E5',
+          accent: '#F59E0B',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
